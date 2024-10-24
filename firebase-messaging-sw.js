0f5164ae62d9840e5048a2c78a2081070c357207
beemerwt <beemerwt@gmail.com>
2024-10-24 08:44:41 -0500

    added config to sw

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,17 @@
+import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging/sw";
 import { onBackgroundMessage } from "firebase/messaging/sw";
 
-const messaging = getMessaging();
+const firebaseApp = initializeApp({
+	apiKey: "AIzaSyDE9ScBcjn3cWe3QAkfJjhAqml_zx4L8lw",
+	authDomain: "tl-notifications-7457a.firebaseapp.com",
+	projectId: "tl-notifications-7457a",
+	storageBucket: "tl-notifications-7457a.appspot.com",
+	messagingSenderId: "172526363830",
+	appId: "1:172526363830:web:68d8cf2a83227d4acdb217"
+});
+
+const messaging = getMessaging(firebaseApp);
 onBackgroundMessage(messaging, (payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
